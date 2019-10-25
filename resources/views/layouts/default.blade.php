b031465ed0d3c282c5eeb038102fe722a5b15914
lzw <641011970@qq.com>
2019-10-25 17:19:19 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,10 @@
     @include('layouts._header')
     <div class="container">
 {{--        @include('shared._messages')--}}
-      @yield('content')
-      @include('layouts._footer')
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
 </body>
 </html>
