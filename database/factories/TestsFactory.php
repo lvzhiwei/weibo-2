fff31acc08ae560e6714fbb7bc5bb5f415a0add9
vagrant <vagrant@homestead>
2019-10-29 02:41:28 +0000

    Move user model to models folders

---
+++ database/factories/TestsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Tests;
+use Faker\Generator as Faker;
+
+$factory->define(Tests::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
